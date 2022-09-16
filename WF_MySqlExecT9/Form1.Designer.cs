4e440c87cbac7a12fd573facdc4dac06a82bd235
Aluno <Aluno@SCS06>
2022-09-15 21:59:46 -0300

    Adicionar arquivos de projeto.

---
+++ WF_MySqlExecT9/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WF_MySqlExecT9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.nome_completo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.data_nascimento = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.telefone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ddd = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.endereco = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.complemento = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cep = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(632, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Buscar Clientes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nome_completo,
+            this.data_nascimento,
+            this.telefone,
+            this.ddd,
+            this.endereco,
+            this.complemento,
+            this.cep,
+            this.id});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(22, 82);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(622, 239);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // nome_completo
+            // 
+            this.nome_completo.Text = "nome_completo";
+            this.nome_completo.Width = 106;
+            // 
+            // data_nascimento
+            // 
+            this.data_nascimento.Text = "data_nascimento";
+            this.data_nascimento.Width = 80;
+            // 
+            // telefone
+            // 
+            this.telefone.Text = "telefone";
+            this.telefone.Width = 94;
+            // 
+            // ddd
+            // 
+            this.ddd.Text = "ddd";
+            this.ddd.Width = 49;
+            // 
+            // endereco
+            // 
+            this.endereco.Text = "endereco";
+            this.endereco.Width = 73;
+            // 
+            // complemento
+            // 
+            this.complemento.Text = "complemento";
+            this.complemento.Width = 86;
+            // 
+            // cep
+            // 
+            this.cep.Text = "cep";
+            this.cep.Width = 47;
+            // 
+            // id
+            // 
+            this.id.Text = "id";
+            this.id.Width = 35;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 333);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader nome_completo;
+        private System.Windows.Forms.ColumnHeader data_nascimento;
+        private System.Windows.Forms.ColumnHeader telefone;
+        private System.Windows.Forms.ColumnHeader ddd;
+        private System.Windows.Forms.ColumnHeader endereco;
+        private System.Windows.Forms.ColumnHeader complemento;
+        private System.Windows.Forms.ColumnHeader cep;
+        private System.Windows.Forms.ColumnHeader id;
+    }
+}
+
